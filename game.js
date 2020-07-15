58faf935d6e00eeae0150efe9bfd1f393d782a13
daddybiggboii <daddybiggboii@gmail.com>
2020-07-15 13:49:17 +1000

    added function for recalling the state of any cell in the grid

--- game.js
+++ game.js
@@ -4,9 +4,10 @@ let gridSize;
 function setup() {
   gridSize = createVector(500, 500);
   createCanvas(gridSize.x, gridSize.y);
+  currentMap = new grid(50);
 }
 
-function newGrid(cellSize) {
+function constructGrid(cellSize) {
   let grid = [];
   for (let cellX = 0; cellX < floor(gridSize.x/cellSize); cellX++) {
     let column = [];
@@ -18,6 +19,24 @@ function newGrid(cellSize) {
   return grid;
 }
 
+function grid(cellSize) {
+  this.cellSize = cellSize;
+  this.grid = constructGrid(cellSize);
+
+  this.state = function(cellX, cellY) { // returns the state
+    return this.grid[cellX][cellY];
+  }
+
+  this.display = function() {
+    for (let X = 0;  X < this.grid.length; X++) {
+      for (let Y = 0;  Y < this.grid[X].length; Y++) {
+        rect(X * this.cellSize, Y * this.cellSize, this.cellSize, this.cellSize);
+      }
+    }
+  }
+}
+
 function draw() {
-  background(0);
+  //background(0);
+  currentMap.display();
 }

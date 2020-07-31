bfb5e99f9381c71a31066669b0834f7b59f15e25
daddybiggboii <daddybiggboii@gmail.com>
2020-07-31 13:06:29 +1000

    undoing

--- game.js
+++ game.js
@@ -28,17 +28,113 @@ function keyPressed() {
 
 }
 
-const immediateNeighbourhood = new Neighbourhood([[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]);
+function constructGrid(rows, columns) { // creates a new 2d array using for loops
+  let grid = [];
+  for (let cellX = 0; cellX < rows; cellX++) {
+    let column = [];
+    for (let cellY = 0; cellY < columns; cellY++) {
+      column.push(false); // adds the default state false into the columns
+    }
+    grid.push(column);
+  }
+  return grid;
+}
+
+class Neighbourhood {
+  constructor(neighbours) { //takes an array of neighbours converts them to vectors
+    this.neighbours = [];
+    for (let neighbourIndex in neighbours) {
+      this.neighbours.push( createVector(neighbours[neighbourIndex][0], neighbours[neighbourIndex][1]) );
+    }
+  }
+  add(x, y) {
+    this.neighbours.push(createVector(x, y));
+  }
+}
+
+class Rule {
+  constructor(initialState, lowerBound, upperBound, stateOfCells, finalState) { //
+    this.initialState = initialState;
+    this.lowerBound = lowerBound;
+    this.upperBound = upperBound;
+    this.stateOfCells = stateOfCells;
+    this.finalState = finalState;
+  }
+  check(x, y, grid) { //returns whether to apply the rule and what state it should be
+    let output = [false, this.initialState];
+    if (grid.checkCell(x, y) === this.initialState && this.lowerBound <= grid.neighbours(x, y, this.stateOfCells) && grid.neighbours(x, y, this.stateOfCells) <= this.upperBound) {
+      output = [true, this.finalState];
+    }
+    return output;
+  }
+}
+
+class RuleSet {
+  constructor(state, rules) {
+    this.rules = rules;
+    this.states = {}; // adds a record of what the states are
+    for (let i in state) {
+      this.states[state[i][0]] = state[i][1];
+    }
+
+  }
+  addRule(rule) {
+    this.rules.push(rule);
+  }
+  checkRules(x, y, grid) {
+    for (let i in this.rules) {
+      if (this.rules[i].check(x, y, grid)[0]) {
+        return (this.rules[i].check(x, y, grid)[1]);
+        break;
+      }
+    }
+    return grid.checkCell(x, y);
+  }
+}
+
 
 //game of life
 
 let deathByLonelyness = new Rule(true, 0, 1, true, false);
 let deathByCrowding = new Rule(true, 4, 8, true, false);
 let birth = new Rule(false, 3, 3, true, true);
-const GOL = new RuleSet([['dead', 'white'], ['alive', 'black']], [birth, deathByCrowding, deathByLonelyness], immediateNeighbourhood);
+let GOL = new RuleSet([['dead', 'white'], ['alive', 'black']], [birth, deathByCrowding, deathByLonelyness]);
 
 // rule 30
 
+
+class Grid {
+  constructor(rows, columns){
+    this.cells = constructGrid(rows, columns);
+    this.neighbourhood = new Neighbourhood([[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]); // temporary
+  }
+  addCell(x, y, state) {
+    this.cells[x][y] = state;
+  }
+  checkCell(x, y) {
+    return this.cells[x][y];
+  }
+  flipCellState(x, y) {
+    this.cells[x][y] = !this.cells[x][y];
+  }
+
+  neighbours(x, y, state) { // returns the amount of neighbours of a given cell in the grid
+    //neighbourhood = [createVector(-1, -1), createVector(-1, 0), createVector(-1, 1), createVector(0, -1), createVector(0, 1), createVector(1, -1), createVector(1, 0), createVector(1, 1)];
+    let amountOfNeighbours = 0;
+    let cellPos = createVector(x, y);
+    let cell;
+
+    for (cell in this.neighbourhood.neighbours) {
+      let neighbour = p5.Vector.add(cellPos, this.neighbourhood.neighbours[cell]);
+      if (neighbour.x >= 0 && neighbour.y >= 0 && neighbour.x < this.cells.length && neighbour.y < this.cells.length && this.checkCell(neighbour.x, neighbour.y) === state) {
+        amountOfNeighbours++;
+      }
+    }
+
+    return amountOfNeighbours;
+
+  }
+}
 
 function setup() {
   gridSize = createVector(0.7*windowWidth, windowHeight);
@@ -58,7 +154,7 @@ function setup() {
 
 function grid(cellSize, ruleSet) {
   this.ruleSet = ruleSet;
-  this.grid = new Grid(floor(gridSize.x/cellSize), floor(gridSize.y/cellSize), ruleset.neighbourhood);
+  this.grid = new Grid(floor(gridSize.x/cellSize), floor(gridSize.y/cellSize));
   this.cellSize = cellSize;
 
   this.iterate = () => {
